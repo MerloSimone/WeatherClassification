aad24f9ce64376cf3ddc14daa442793cfb0a711c
MerloSimone <76220305+MerloSimone@users.noreply.github.com>
2024-01-23 18:08:48 +0100

    Add files via upload

--- featureextractor.py
+++ featureextractor.py
@@ -3,11 +3,32 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
+
+"""
+Loader: class used to load a dataset composed of images. it loads the images, computes and returns a descriptor for each image along with the corresponding class labels
+
+""" 
 class Loader:
 
+
     def __init__(self, dataset :str, datapath :str, bin_num :int =256, use_gradient :bool =False, use_sharpness :bool = True, use_luminance :bool =True):
+        """
+        Creates an image loader.
+    
+        Args:
+            dataset (str): name of the dataset to be used eg. MWD
+            datapath (str): path of the data to be loaded eg. ./data/MWD/training
+            bin_num (int): number of bins that will be used in the histogram computation (quantize the level axis of the histogram)
+            use_gradient (bool): flag to choose to use or note the gradient in the descriptors
+            use_sharpness (bool): flag to choose to use or note the sharpness in the descriptors
+            use_luminance (bool): flag to choose to use or note the luminance in the descriptors
+
+        """ 
+
+
         self.datapath=datapath
 
+        #ADDING a slash at the end of the path if not present
         if(not self.datapath.endswith('/')):
             self.datapath=self.datapath+'/'
 
@@ -15,12 +36,17 @@ class Loader:
         self.dataset=dataset
         self.bin_num=bin_num
 
+        #checking that the provided number of bins is valid
         if(self.bin_num <= 0 or bin_num > 256):
             raise Exception('Invalid number of bins {}'.format(bin_num))
 
+        #checking that the provided dataset is valid
         if(dataset not in self.datasets):
             raise Exception('Wrong dataset provided, chose from MWD,ACDC,UAVid,syndrone')
 
+
+        #selecting the classes based on the chosen dataset
+        #ATTENTION: classes represents the label that will be given to the images while candidates represents the strings that will be compared with the image names to assing the labels
         if dataset == self.datasets[0]:
             self.classes=['cloudy','rain','shine','sunrise']
             self.candidates=['cloudy','rain','shine','sunrise']
@@ -35,18 +61,32 @@ class Loader:
             self.candidates=['ClearNoon','MidFoggyNoon','ClearNight','HardRainNoon']
 
 
+        #setting flags
         self.use_gradient=use_gradient
         self.use_sharpness=use_sharpness
         self.use_luminance=use_luminance
 
 
 
-    def get_classes(self):
+    def getClasses(self) -> list :
+        """
+        Returns the classes of the dataset.
+    
+        Returns:
+            classes (list): list of the distinct classes in the dataset
+
+        """ 
         return self.classes
 
 
-    def get_class(self,image_name :str):
-        
+    def getClass(self,image_name :str) -> str:
+        """
+        Returns the class given the name of an image.
+    
+        Returns:
+            class (str): class of the image based on the image name
+
+        """ 
 
         for i in range(len(self.candidates)):
             if(self.candidates[i] in image_name):
@@ -59,11 +99,20 @@ class Loader:
 
 
 
-    def load_images(self):
+    def loadImages(self) -> tuple[np.array,np.array]:
+        """
+        loads the images returning  the arrays corresponfing to the image descriptors and the classes for each image
+    
+        Returns:
+            X (numpy.array): matrix having in each row the descriptor of an image
+            y (numpy.array): array having in each position the label of the corresponding image in X
+        """ 
+
+        #Choosing which method to used based on the dataset
         if self.dataset == 'MWD':
-            return self.load_mwd()
+            return self.loadMwd()
         else:
-            return self.load_other()
+            return self.loadOther()
 
 
 
@@ -73,32 +122,47 @@ class Loader:
 
 
 
-#TBD: da personalizzare i vari metodi per una corretta classificazione.
 
 
 
 
-    def load_mwd(self):
+    def loadMwd(self) -> tuple[np.array,np.array]:
+
+        """
+        loads the images returning  the arrays corresponfing to the image descriptors and the classes for each image.
+        method used only for the MWD dataset since it has a particular folder structure.
+    
+        Returns:
+            X (numpy.array): matrix having in each row the descriptor of an image
+            y (numpy.array): array having in each position the label of the corresponding image in X
+        """ 
         X=np.array([])
         y=np.array([])
         
+
         for cls in self.classes:
-            
+
+            #for each class, load all the images in the corresponfing folder
+
             tmp_path=self.datapath+cls
             files_list = os.listdir(tmp_path)
-            for file in files_list:
-                #print(file)
-                image=cv.imread(tmp_path+'/'+file)
-                if(image is not None) :
-                    #COMPUTE FEATURES AND APPEND DATA TO X and y
-                    #cv.imshow('test0',image)
-                    #cv.waitKey(1)
-                    image_feat=self.get_features(image)
-                    #print(image_feat)
-                    #print(image_feat.shape)
-                    image_cls=self.get_class(file)
-                    #print(image_cls)
 
+            #load each file
+            for file in files_list:
+                #load image
+                image=cv.imread(tmp_path+'/'+file)
+
+                #computing descriptors only if the file is actually an image
+                if(image is not None) :
+                    
+                    #getting descriptors
+                    image_feat=self.getFeatures(image)
+                    
+                    #getting class label
+                    image_cls=self.getClass(file)
+                    
+
+                    #appending label and descriptor to the matrices
 
                     y=np.append(y,image_cls)
 
@@ -113,25 +177,38 @@ class Loader:
         return X,y
 
 
-    def load_other(self):
+    def loadOther(self) -> tuple[np.array,np.array]:
+        """
+        loads the images returning  the arrays corresponfing to the image descriptors and the classes for each image
+    
+        Returns:
+            X (numpy.array): matrix having in each row the descriptor of an image
+            y (numpy.array): array having in each position the label of the corresponding image in X
+        """ 
+
         X=np.array([])
         y=np.array([])
+
         path=self.datapath
         files_list = os.listdir(path)
+
+        #load each file
         for file in files_list:
-            #print(file)
+            
+            #load image
             image=cv.imread(path+'/'+file)
+
+            #computing descriptors only if the file is actually an image
             if(image is not None) :
-                #COMPUTE FEATURES AND APPEND DATA TO X and y 
-                #cv.imshow('test0',image)
-                #cv.waitKey(1)
-                image_feat=self.get_features(image)
-                #print(image_feat)
-                #print(image_feat.shape)
-                image_cls=self.get_class(file)
-                #print(image_cls)
 
+                #getting descriptors
+                image_feat=self.getFeatures(image)
+                
+                #getting class label
+                image_cls=self.getClass(file)
+                
 
+                #appending label and descriptor to the matrices
 
                 y=np.append(y,image_cls)
 
@@ -148,48 +225,128 @@ class Loader:
 
 
 
-    def get_features(self,image :np.ndarray):
-        feat=self.compute_histograms(image)
+    def getFeatures(self,image :np.ndarray) -> np.array:
+        """
+        Computes the descriptor of a given image
+
+        Args:
+            image (numpy.ndarray): the image to be described 
+    
+        Returns:
+            feat (numpy.array): array rapresenting the image descriptor containing the features
+        """ 
+
+        #computing the features based on the histograms
+        feat=self.computeHistograms(image)
+
+        #computing other auxiliary features based on the provided flags
         if(self.use_sharpness):
-            feat=np.append(feat,self.compute_sharpness(image))
+            feat=np.append(feat,self.computeSharpness(image))
         if(self.use_gradient):
-            feat=np.append(feat,self.compute_gradient(image))
+            feat=np.append(feat,self.computeGradient(image))
         if(self.use_luminance):
-            feat=np.append(feat,self.compute_luminance(image))
+            feat=np.append(feat,self.computeLuminance(image))
         return feat
 
-    def compute_histograms(self,image: np.ndarray):
+
+
+
+
+
+
+    def computeHistograms(self,image: np.ndarray) -> np.array:
+        """
+        Computes the histograms of a given image
+
+        Args:
+            image (numpy.ndarray): the image  
+    
+        Returns:
+            image_features (numpy.array): array rapresenting the histogram of the image concatenated in a single array
+        """ 
         
         if image is None:
             raise Exception('Invalid None image')
 
+        #computing histograms based on the number of bins provided as a parameter of the class
         image_features=np.array([])
         channel_0 = cv.calcHist([image[:,:,0]],[0],None,[self.bin_num],[0,256]).squeeze()
         channel_1 = cv.calcHist([image[:,:,1]],[0],None,[self.bin_num],[0,256]).squeeze()
         channel_2 = cv.calcHist([image[:,:,2]],[0],None,[self.bin_num],[0,256]).squeeze()
+
+        #concatenating the histograms of the different channels
         image_features=np.append(image_features,channel_0)
         image_features=np.append(image_features,channel_1)
         image_features=np.append(image_features,channel_2)
+
         return image_features
 
 
-    def compute_sharpness(self,image: np.ndarray):
+
+
+
+
+    def computeSharpness(self,image: np.ndarray):
+        """
+        Computes the sharpness of a  given image exploiting the laplacian
+
+        Args:
+            image (numpy.ndarray): the image to be described 
+    
+        Returns:
+            sharpness (float): the average value of the sharpness of the image
+        """ 
+
+        #Converting image to grayscale
+        image=cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+        #computing the laplacian
         laplacian=cv.Laplacian(image,-1)
+
+        #computing the sharpness as mean of the laplacian over the entire image (anc over all the channels)
         sharpness=np.mean(laplacian)
+
         return sharpness
 
 
-    def compute_gradient(self,image: np.ndarray):
+    def computeGradient(self,image: np.ndarray):
+        """
+        Computes the gradient of a  given image exploiting the Sobel masks
+
+        Args:
+            image (numpy.ndarray): the image to be described 
+    
+        Returns:
+            mag (float): the average value of the gradient of the image
+        """ 
         
+        #Converting image to grayscale
+        image=cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+        #Computing the derivatives in both direction exploiting the sobel mask
         dx=cv.Sobel(image,cv.CV_64F,1,0)
         dy=cv.Sobel(image,cv.CV_64F,0,1)
+
+        #computing the gradient magnitude based on the derivatives
         mag=cv.magnitude(dx,dy)
 
         return np.mean(mag)
 
-    def compute_luminance(self,image: np.ndarray):
+    def computeLuminance(self,image: np.ndarray):
+        """
+        Computes the luminance of a  given image exploiting the LAB representation
+
+        Args:
+            image (numpy.ndarray): the image to be described 
+    
+        Returns:
+            luminance (float): the average value of the luminance of the image
+        """
+        #converting image
         Lab_image=cv.cvtColor(image, cv.COLOR_BGR2LAB)
+
+        #computing average of the L (luminance) channel
         luminance=np.mean(Lab_image[:,:,0])
+
         return luminance
         
 
--- weatherclassifier.py
+++ weatherclassifier.py
@@ -5,9 +5,26 @@ from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
 
 
+"""
+WeatherClassifier: class used to train/test a MLP Classifier based on the training and test sets provided.
+
+""" 
 class WeatherClassifier:
     
     def __init__(self, X:np.ndarray, y: np.ndarray,random_seed=1, max_iter=200):
+        
+        """
+        Creates a weather classifier.
+    
+        Args:
+            X (numpy.ndarray): training set having the descriptor for each image on each row
+            y (numpy.ndarray): array of the image labels ordered according to X
+            random_seed (int): random seed to be used to achieve invariance accross multiple executions
+            max_iter (int): maximum number of iteration to train the classifier
+
+        """ 
+
+        #checking the parameters
         if( (X is None) or (y is None)):
             raise Exception('X or y are none')
 
@@ -17,68 +34,132 @@ class WeatherClassifier:
         elif(max_iter<=0):
             raise Exception('Wrong maximum number of iterations (max_iter) provided, must be grater than 0')
 
+
         self.X=X
         self.y=y
+
+        #creating a standard scaler to scale the features
         self.scaler = StandardScaler().fit(self.X)
+
+        #creating a MLP classifier with the parameters provided
         self.mlp_classifier=MLPClassifier(random_state=random_seed,max_iter=max_iter)
+
         self.random_seed=random_seed
         self.max_iter=max_iter
+
+        #setting random seeds
         np.random.seed(self.random_seed)
 
 
 
 
-    def permute(self,X: np.ndarray, y: np.ndarray):
+    def permute(self,X: np.ndarray, y: np.ndarray) -> tuple[np.ndarray,np.ndarray]:
+        """
+        Performs a random permutation on the provided data.
+    
+        Args:
+            X (numpy.ndarray): dataset having the descriptor for each image on each row
+            y (numpy.ndarray): array of the image labels ordered according to X
+        Returns:
+            X_perm (numpy.ndarray): permuted verision of X
+            y_perm (numpy.ndarray): permuted version of y
+
+        """ 
         if( (X is None) or (y is None)):
             raise Exception('X or y are none')
 
+        #creating a permutation
         permutation = np.random.permutation(X.shape[0])
+
+        #permuting data
         X_perm = X[permutation]
         y_perm = y[permutation]
         return X_perm,y_perm
 
 
-    def scale(self,X: np.ndarray):
+    def scale(self,X: np.ndarray) -> np.ndarray:
+        """
+        Scales data according to the training set .
+    
+        Args:
+            X (numpy.ndarray): training set having the descriptor for each image on each row
+        Returns:
+            X_scaled (numpy.ndarray): scaled verision of X
+
+        """
+
         if(X is None):
             raise Exception('X is none')
 
+        #scaling data using the scaler previously fitted on the training data
         X_scaled = self.scaler.transform(X)
 
         return X_scaled
 
 
-    def train_classifier(self):
+    def trainClassifier(self) -> MLPClassifier:
+        """
+        Trains the MLPClassifier according to the training set .
+    
+        Returns:
+            mlp_classifier (MLPClassifier): trained classifier
 
+        """
+
+        
         self.mlp_classifier=MLPClassifier(random_state=self.random_seed,max_iter=self.max_iter)
+
+        #permuting and scaling data
         X_perm,y_perm=self.permute(self.X,self.y)
         X_perm_scaled=self.scale(X_perm)
+
+        #training the classifier
         self.mlp_classifier.fit(X_perm_scaled,y_perm)
 
         return self.mlp_classifier
 
 
 
-    def score(self,X_test: np.ndarray, y_test:np.ndarray):
+    def score(self,X_test: np.ndarray, y_test:np.ndarray) -> float:
+        """
+        Test the provided dataset on the MLPClassifier and returns the score.
+    
+        Returns:
+            score (float): the score obtained with the provided dataset
 
+        """
+
+        #scale data
         X_test_scaled=self.scale(X_test)
 
         return self.mlp_classifier.score(X_test_scaled,y_test)
 
 
 
-    def grid_search(self):
+    def gridSearch(self) -> tuple[MLPClassifier,float]:
+        """
+        Trains the MLPClassifier according to the training set, exploiting grid search to find the best parameter combination.
+    
+        Returns:
+            best_estimator_ (MLPClassifier): trained classifier (with the best parameter combination)
+            best_score_ (float): best classifier score
+        """
 
         classifier=MLPClassifier()
 
+        #permuting and scaling data
         X_perm,y_perm=self.permute(self.X,self.y)
         X_perm_scaled=self.scale(X_perm)
 
+
+        #creating a grid of parameter to test different layers/neuron combinations
         param_grid = {'hidden_layer_sizes': [(10,),(20,),(100,),(10,10),(100,100)],
                     'solver': ['lbfgs', 'adam'], 
                     'random_state': [self.random_seed],
                     'max_iter': [self.max_iter]
                     }
         
+        #performing grid search
         grid = GridSearchCV(classifier, param_grid=param_grid, cv=5, verbose=4)
         grid.fit(X_perm_scaled, y_perm)
 
